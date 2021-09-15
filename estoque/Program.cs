663047b8b926dfe368cd9234830ad49a6d641651
MAUAK <kauamsilva360@gmail.com>
2021-09-15 20:02:59 -0300

    atualização

--- estoque/Program.cs
+++ estoque/Program.cs
@@ -29,14 +29,14 @@ namespace estoque
             p.AdicionarProduto(qte);
 
             //Escrevendo os dados do produto, pegando as respostas das variaveis da instancia p
-            Console.WriteLine("Dados do atualizados: " + p);
+            Console.WriteLine("Dados do produto atualizados: " + p);
 
             //Pedindo para o usuário informar a quantidade de produtos removidos e chamando o método remover produtos com o parametro da quantidade que o usuário informou.
             Console.WriteLine("Digite o número de produtos a serem removidos do estoque: ");
             qte = int.Parse(Console.ReadLine());
             p.RemoverProduto(qte);
             //Escrevendo os dados do produto, pegando as respostas das variaveis da instancia p
-            Console.WriteLine("Dados do atualizados: " + p);
+            Console.WriteLine("Dados do produto atualizados: " + p);
         }
     }
 }
