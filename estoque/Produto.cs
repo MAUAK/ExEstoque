ed398a753dbd7dff305d3879ef43c983529b4676
MAUAK <kauamsilva360@gmail.com>
2021-09-22 19:47:44 -0300

    Encapsulamento.

--- estoque/Produto.cs
+++ estoque/Produto.cs
@@ -8,20 +8,11 @@ namespace estoque
     class Produto
     {
         //Criando a variavel Nome do produto
-        public string Nome;
+        private string _nome;
         //Criando a variavel Preco do produto
-        public double Preco;
+        private double _preco;
         //Criando a variavel Quantidade do produto
-        public int Quantidade;
-
-        //Criando construtor Produto e passando como parametro variaveis nome, preco e quantidade
-        public Produto  (string nome, double preco, int quantidade)
-        {
-            //Igualando as variaveis da classe Produto com as variaveis do método Produto
-            Nome = nome;
-            Preco = preco;
-            Quantidade = quantidade;
-        }
+        private int _quantidade;
 
         //Criando um construtor vázio
         public Produto()
@@ -29,29 +20,67 @@ namespace estoque
 
         }
 
+        //Criando construtor Produto e passando como parametro variaveis nome, preco e quantidade
+        public Produto  (string nome, double preco, int quantidade)
+        {
+            //Igualando as variaveis da classe Produto com as variaveis do método Produto
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        public void SetNome(string nome)
+        {
+            _nome = nome;
+        }
+        
+        public double  GetPreco()
+        {
+            return _preco;
+        }
+
+        public void SetPreco(double preco)
+        {
+            _preco = preco;
+        }
+        public int GetQuantidade()
+        {
+            return _quantidade;
+        }
+
+        public void SetQuantidade(int quantidade)
+        {
+            _quantidade = quantidade;
+        }
+
         //Criando o método´para retornas o valor total do produto em estoque
         public double ValorTotalEmEstoque() 
         { 
-            return Preco * Quantidade; 
+            return _preco * _quantidade; 
         }
 
         //Criando método para adicionar a quantidade (que o usuario quiser) de produtos na variavel Quantidade
         public void AdicionarProduto(int quantidade) 
         { 
-            Quantidade += quantidade; 
+            _quantidade += quantidade; 
         }
 
         //Criando método para remover a quantidade (que o usuario quiser) de produtos na variavel Quantidade
         public void RemoverProduto(int quantidade) 
         { 
-            Quantidade = Quantidade - quantidade; 
+            _quantidade = _quantidade - quantidade; 
         }
 
         //retornando os dados das variaveis da classe Produto
         public override string ToString() 
         { 
-            return Nome + ", R$ " + Preco.ToString("F2", CultureInfo.InvariantCulture)
-                + ", " + Quantidade + " unidades, Total: R$ " 
+            return _nome + ", R$ " + _preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", " + _quantidade + " unidades, Total: R$ " 
                 + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture); }
     }
 }
